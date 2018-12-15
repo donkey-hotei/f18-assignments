4046d331dd836abef5822c5f5fa44fe832c51403
donkey-hotei <donkey_hotei@cryptolab.net>
2018-12-15 11:50:03 -0800

    finish lab1 exercise 2

--- lab1/exercise2.ml
+++ lab1/exercise2.ml
@@ -19,13 +19,27 @@ module IntTreeImpl : IntTree = struct
     | Node (l, _, r) -> 1 + (size l) + (size r)
 
   let rec map (f : int -> int) (t : tree) =
-    raise Unimplemented
+    match t with
+    | Leaf -> t
+    | Node (l, v, r) -> Node(map f l, f v, map f r)
 
   let rec to_string (t : tree) : string =
-    raise Unimplemented
+    match t with
+      | Leaf -> "()"
+      | Node (l, v, r) -> 
+        Printf.sprintf "(%s, %d, %s)" 
+        (to_string l)  v (to_string r)
 
   let rec binary_search (t : tree) (n : int) : int option =
-    raise Unimplemented
+    match t with
+      | Leaf -> None
+      | Node (l, v, r) -> 
+        if n < v
+          then binary_search l n
+        else if n = v then Some v
+        else match r with
+          | Leaf -> Some v
+          | Node(_, _, _) -> binary_search r n
 end
 
 module IntTreeTests(T : IntTree) = struct
@@ -35,6 +49,7 @@ module IntTreeTests(T : IntTree) = struct
   assert (T.map (fun x -> x + 1) t = T.Node(T.Leaf, 3, T.Leaf));
 
   let t = T.Node(T.Node(T.Leaf, 1, T.Leaf), 3 ,T.Node(T.Leaf, 5, T.Leaf)) in
+
   assert (T.binary_search t 2 = Some 1);
   assert (T.binary_search t 3 = Some 3);
   assert (T.binary_search t 6 = Some 5);
